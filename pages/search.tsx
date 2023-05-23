898b56fb2c284bb4799f3a2bbc8c298057d2ee65
Rosie <rosie.tianshen@gmail.com>
2023-05-23 15:26:40 -0700

    Added card elements for saved items

--- pages/search.tsx
+++ pages/search.tsx
@@ -336,34 +336,46 @@ function SearchPage(){
 
 
     return(
-        <div>
-            {/* add form instead of div for auto refresh? */}
-     <div
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        // onSubmit={search}
+      <div>
+          {/* add form instead of div for auto refresh? */}
+      <div
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      // onSubmit={search}
       >
-        <label className="block text-gray-700 text-sm font-bold mb-2">
+      <label className="block text-gray-700 text-sm font-bold mb-2">
+        Search
+      </label>
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="Search"
+        type="text"
+        placeholder="Username"
+        value={searchParam}
+        onChange={handleSearchChange}
+      />
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+          onClick={search}
+        >
           Search
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="Search"
-          type="text"
-          placeholder="Username"
-          value={searchParam}
-          onChange={handleSearchChange}
-        />
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-            onClick={search}
-          >
-            Search
-          </button>
-        </div>
-    </div>
-        </div>
+        </button>
+      </div>
+{/*  */}
+      <div>
+        {userData.map((item, index) => (
+          <div key={index} className="bg-white shadow-md rounded p-4 mb-4">
+            <h3 className="text-gray-600">{item.name}</h3>
+            <p className="text-gray-600">{item.genres}</p>
+            <p className="text-gray-600">{item.type}</p>
+            <img src={item.images[0]} alt={item.name} className="w-20 h-20 mt-2" />
+          </div>
+        ))}
+      </div>
+
+      </div>
+      </div>
     );
 }
 
