6582ced7e53ae6e19f61ea1776f75b4fae53f152
Rosie <rosie.tianshen@gmail.com>
2023-05-23 20:28:14 -0700

    Added search filter functionality

--- pages/search.tsx
+++ pages/search.tsx
@@ -11,89 +11,25 @@ const CLIENT_SECRET = "4f28abe2fa6b4d2cb274afb4c650d38c";
 
 
 function SearchPage(){
-
-  
-
     const [accessToken, setAccessToken] = useState("");
-    // const [refreshToken, setRefreshToken] = useState("");
+    const [filteredUserData, setFilteredUserData] = useState<{ name: any; images: any; genres: any; type: string; }[]>([]);
     const [searchParam, setSearchParam] = useState("");
     const [userData, setUserData] = useState<{ name: any; images: any; genres: any; type: string; }[]>([]);
 
 
-    const handleSearchChange = (event: any) => {
-        setSearchParam(event.target.value);
-        console.log(searchParam);
-      };
-  
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setSearchParam(value);
+      
+      const filteredData = userData.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
+      setFilteredUserData(filteredData);
+    };
     
-
-    // // refresh the token upon 401 error
-    // useEffect(() => {
-    //   const getAccessToken = async () => {
-    //     try {
-    //       const authParams = {
-    //         method: 'POST',
-    //         headers: {
-    //           'Content-Type': 'application/x-www-form-urlencoded'
-    //         },
-    //         body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
-    //       };
-  
-    //       const response = await fetch('https://accounts.spotify.com/api/token', authParams);
-    //       const data = await response.json();
-    //       setAccessToken(data.access_token);
-    //       console.log('access token:', accessToken);
-    //       // fetchSavedData();
-    //     } catch (error) {
-    //       // Handle error here 
-    //       console.error('Error fetching access token:', error);
-    //     }
-    //   };
-  
-    //   getAccessToken();
-    // }, []);
+    useEffect(() => {
+      setFilteredUserData(userData);
+    }, [userData]);
     
-
-    // useEffect(() => {
-    //   const fetchSavedData = async () => {
-    //     try {
-    //       const params = {
-    //         headers: {
-    //           Authorization: `Bearer ${accessToken}`
-    //         }
-    //       };
   
-    //       const trackResponse = await fetch('https://api.spotify.com/v1/me/tracks', params);
-    //       const trackData = await trackResponse.json();
-    //       parseTrackData(trackData.items);
-  
-    //       const albumResponse = await fetch('https://api.spotify.com/v1/me/albums', params);
-    //       const albumData = await albumResponse.json();
-    //       parseAlbumData(albumData.items);
-  
-    //       const episodeResponse = await fetch('https://api.spotify.com/v1/me/episodes', params);
-    //       const episodeData = await episodeResponse.json();
-    //       parseEpisodeData(episodeData.items);
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   };
-  
-    //   if (accessToken) {
-    //     fetchSavedData();
-    //   }
-    // }, [accessToken]);
-
-    // function refreshToken(){
-    //   console.log("refreshin?");
-    //   if (window.location.hash) {
-    //     const { access_token, expires_in, token_type } = getReturnedParamsFromSpotifyAuth(window.location.hash);
-    //     setAccessToken(access_token);
-    //     localStorage.setItem('accessToken', access_token);
-    //     fetchSavedData();
-    //   }
-    // }
-
     useEffect(() => {
       const storedAccessToken = localStorage.getItem('accessToken');
       if (storedAccessToken) {
@@ -178,12 +114,6 @@ function SearchPage(){
       return paramsSplitUp;
     };
 
-  
-
-
-
-
-
   function parseTrackData(data: any) {
   console.log("parsing track data");
   console.log(data);
@@ -222,219 +152,61 @@ function SearchPage(){
 }
 
     
-    
-    function parseAlbumData(data: any) {
-      console.log("parsing data");
-      console.log(data);
-    
-      if (Array.isArray(data)) {
-        data.forEach((item: any) => {
-          const { name, genres, images } = item.album;
-    
-          // Check if the item already exists in userData
-          const isDuplicate = userData.some((existingItem) => existingItem.name === name);
-    
-          // Add the item to userData if it's not a duplicate
-          if (!isDuplicate) {
-            const newItem = {
-              name,
-              images,
-              genres,
-              type: 'album'
-            };
-    
-            setUserData((prevData: any) => [...prevData, newItem]);
-          }
-        });
-      } else {
-        console.log("Invalid data format. Expected an array.");
-      }
-    }
-
-    function parseEpisodeData(data: any) {
-      console.log("parsing data");
-      console.log(data);
-    
-      if (Array.isArray(data)) {
-        data.forEach((item: any) => {
-          const { name, genres, images } = item.episode;
-    
-          // Check if the item already exists in userData
-          const isDuplicate = userData.some((existingItem) => existingItem.name === name);
-    
-          // Add the item to userData if it's not a duplicate
-          if (!isDuplicate) {
-            const newItem = {
-              name,
-              images,
-              genres,
-              type: 'episode'
-            };
-    
-            setUserData((prevData: any) => [...prevData, newItem]);
-          }
-        });
-      } else {
-        console.log("Invalid data format. Expected an array.");
-      }
-    }
-    //
-      
+  function parseAlbumData(data: any) {
+    console.log("parsing data");
+    console.log(data);
   
-    async function getSaved() {
-      console.log("in get saved");
-      console.log(accessToken);
-      var params = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          // 'Authorization': `Bearer ${accessToken}`
-          'Authorization': 'Bearer ' + accessToken
+    if (Array.isArray(data)) {
+      data.forEach((item: any) => {
+        const { name, genres, images } = item.album;
+  
+        // Check if the item already exists in userData
+        const isDuplicate = userData.some((existingItem) => existingItem.name === name);
+  
+        // Add the item to userData if it's not a duplicate
+        if (!isDuplicate) {
+          const newItem = {
+            name,
+            images,
+            genres,
+            type: 'album'
+          };
+  
+          setUserData((prevData: any) => [...prevData, newItem]);
         }
-      };
-    
-      try {
-        var savedTracks = await fetch("https://api.spotify.com/v1/me/tracks", params)
-          .then(response => response.json())
-          .then(data => {
-                console.log(data.items);
-                parseTrackData(data.items);
-          })
-          var savedAlbums = await fetch("https://api.spotify.com/v1/me/albums", params)
-          .then(response => response.json())
-          .then(data => {
-                console.log(data.items);
-                parseAlbumData(data.items);
-          })
-          var savedEpisodes = await fetch("https://api.spotify.com/v1/me/episodes", params)
-          .then(response => response.json())
-          .then(data => {
-                console.log(data.items);
-                // parseEpisodeData(data.items);
-          })
-        // var savedAlbums = await fetch("https://api.spotify.com/v1/me/albums", params)
-        // var savedEpisodes = await fetch("https://api.spotify.com/v1/me/episodes", params)
-
-      } catch (error) {
-        console.log(error);
-      }
+      });
+    } else {
+      console.log("Invalid data format. Expected an array.");
     }
+  }
 
-
-
-    
-      // var trackArray = savedTracks.map(item => ({ track: item.track }));
-      // console.log(trackArray);
-
-    // useEffect(() => {
-    //     // API access Token (running twice in dev mode but should be expected..?)
-    //     var authParams = {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/x-www-form-urlencoded'
-    //         },
-    //         body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    //     }
-    //     // TODO add error handling for fetch later
-    //     fetch('https://accounts.spotify.com/api/token', authParams)
-    //         .then(result => result.json())
-    //         .then(data => setAccessToken(data.access_token))
-        
-    //     // call to retrieve all saved data
-    //     getSaved();
-            
-    // }, [])
-
-
-    async function searchArtists() {
-      console.log("in search artists")
-      console.log(accessToken)
-      var params = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'applications/json',
-          'Authorization': `Bearer ${accessToken}`
+  function parseEpisodeData(data: any) {
+    console.log("parsing data");
+    console.log(data);
+  
+    if (Array.isArray(data)) {
+      data.forEach((item: any) => {
+        const { name, genres, images } = item.episode;
+  
+        // Check if the item already exists in userData
+        const isDuplicate = userData.some((existingItem) => existingItem.name === name);
+  
+        // Add the item to userData if it's not a duplicate
+        if (!isDuplicate) {
+          const newItem = {
+            name,
+            images,
+            genres,
+            type: 'episode'
+          };
+  
+          setUserData((prevData: any) => [...prevData, newItem]);
         }
-      }
-      var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchParam + '&type=artist', params)
-        .then(response => response.json())
-        .then(data => console.log(data))
+      });
+    } else {
+      console.log("Invalid data format. Expected an array.");
     }
-//    serach [] needs to be async since we're gonna have a lot of fetch statements
-    // const handleGetSaved = () => {
-    //   console.log("in handleGetSaved")
-
-    //   var getParams = {
-    //     method: 'GET',
-    //     headers: {
-    //       'Authorization': `Bearer ${accessToken}`
-    //     }
-    // }
-
-
-    //   fetch("https://api.spotify.com/v1/me/tracks", getParams)
-    //     .then(response => {
-    //       if (!response.ok) {
-    //         throw new Error('Network response was not ok');
-    //       }
-    //       return response.json();
-    //     })
-    //     .then(data => {
-    //       setUserData(data);
-    //       console.log("userData:");
-    //       console.log(userData);
-    //     })
-    //     .catch(error => {
-    //       console.log("error");
-    //       console.log(error);
-    //     });
-    // };
-    
-
-    //TODO: make this async?
-    // const handleGetSaved = () => {
-    //     axios
-    //         .get("https://api.spotify.com/v1/me/tracks", {
-    //             headers: {
-    //                 'Authorization': 'Bearer ' + accessToken,
-    //             },
-    //         })
-    //         .then((response : any) => {
-    //             setUserData(response.data);
-    //             console.log(userData);
-    //         })
-    //         .catch((error : any) => {
-    //             console.log(error);
-    //         });
-    // };
-
-
-    // async function getSaved() {
-    //     console.log(accessToken);
-    //     var savedSearchParams = {
-    //       method: 'GET',
-    //       headers: {
-    //         // 'Content-Type': 'application/json',
-    //         'Authorization': 'Bearer ' + accessToken
-    //       }
-    //     };
-      
-    //     // get request to retrieve saved tracks
-    //     try {
-    //       const response = await fetch("https://api.spotify.com/v1/me/tracks", savedSearchParams);
-    //       const data = await response.json();
-    //       console.log(data);
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   }
-    
-    // string matching/search handler
-    async function search(){
-        console.log("searching for " + searchParam);      
-    }
-
-
+  }
 
     return(
       <div>
@@ -456,17 +228,11 @@ function SearchPage(){
         onChange={handleSearchChange}
       />
       <div className="flex items-center justify-between">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-          onClick={search}
-        >
-          Search
-        </button>
+
       </div>
-{/*  */}
+
       <div>
-        {userData.map((item, index) => (
+        {filteredUserData.map((item, index) => (
           <div key={index} className="bg-white shadow-md rounded p-4 mb-4">
             <h3 className="text-gray-600">{item.name}</h3>
             <p className="text-gray-600">{item.genres}</p>
@@ -474,6 +240,7 @@ function SearchPage(){
             <img src={item.images[0].url} alt={item.name} className="w-20 h-20 mt-2" />
           </div>
         ))}
+
       </div>
 
       </div>
